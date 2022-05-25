bfe258a8e2301c629913377304c2cbb5887fed87
Calvin Huynh <ctdh24@gmail.com>
2022-05-24 22:50:52 -0700

    initial

---
+++ GameBacklog/GameBacklog/Data/GameBacklogContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GameBacklog.Models;
+
+namespace GameBacklog.Data
+{
+    public class GameBacklogContext : DbContext
+    {
+        public GameBacklogContext (DbContextOptions<GameBacklogContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GameBacklog.Models.Game>? Game { get; set; }
+    }
+}
---
+++ GameBacklog/GameBacklog/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using GameBacklog.Data;
+using GameBacklog.Models;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new GameBacklogContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<GameBacklogContext>>()))
+            {
+                // Look for any movies.
+                if (context.Game.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Game.AddRange(
+                    new Game
+                    {
+                        Title = "Final Fantasy XIV: A Realm Reborn",
+                        ReleaseDate = DateTime.Parse("2013-8-24"),
+                        Genre = "MMORPG",
+                        Price = 59.99M
+                    },
+
+                    new Game
+                    {
+                        Title = "Phantasy Star Online 2: New Genisis",
+                        ReleaseDate = DateTime.Parse("2012-7-4"),
+                        Genre = "MMORPG",
+                        Price = 0.0M
+                    },
+
+                    new Game
+                    {
+                        Title = "Elden Ring",
+                        ReleaseDate = DateTime.Parse("2022-2-25"),
+                        Genre = "Open World",
+                        Price = 59.99M
+                    },
+
+                    new Game
+                    {
+                        Title = "Genshin Impact",
+                        ReleaseDate = DateTime.Parse("2020-9-28"),
+                        Genre = "Open World",
+                        Price = 0.0M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
